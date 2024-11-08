682e07a9aa191cd3b954f0d21e9c6d55c7cd98a1
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-11-08 12:06:59 +0100

    Update JED.js

--- JED.js
+++ JED.js
@@ -15,21 +15,53 @@ const elementNumbers = [
 
 // List of diet options and elements associated with each diet
 const DietsList = [
-    "Life Elements", "Air Elements", "Critical Elements", 
-    "Smartphone Elements", "DNA Elements", "Radioactive Elements",
-    "Safe Fusion Reactor Elements", "Elements Dedicated to Scientists", 
-    "Latin Derived Elements", "Greek Derived Elements", 
-    "City or Place Named Elements", "Non-latin/Greek/City Derived Names", 
-    "Solid State Elements", "Liquid State Elements", 
-    "Gaseous State Elements", "Metals", "Non-Metals", 
-    "Group I Elements", "Group II Elements", 
-    "Group XV Elements", "Group XVI Elements", 
-    "Group XVII Elements", "Group XVIII Elements", 
-    "Lanthanoids", "Actinoids", "Transition Metals", 
-    "Post-Transition Metals", "Metalloids", "Reactive Non-Metals", 
-    "s-Block Elements", "p-Block Elements", "d-Block Elements", 
-    "f-Block Elements", "Ancient Known Elements"
+    "elementi della vita", "elemeni dell'aria", "elementi critici", "elementi degli smartphone", "elementi del DNA", "elementi radioattivi (serie di decadimento U-Th)",
+    "elementi considerati sicuri (grado A-E)\n in un reattore nucleare a fusione", "elementi dedicati a scienziati", "elementi con nome di derivazione latina",
+    "elementi con nome di derivazione greca", "elementi con nomi di città,\nstati, o di chissà dove", "elementi con nomi che non derivano nè dal latino, nè dal greco,\ne nemmeno da città o stati",
+    "elementi allo stato solido alle condizioni standard di temperatura e pressione", "elementi allo stato liquido alle condizioni standard di temperatura e pressione",
+    "elementi allo stato gassoso alle condizioni standard di temperatura e pressione", "metalli", "non metalli", "elementi del primo gruppo (idrogeno & metalli alcalini)",
+    "elementi del secondo gruppo (metalli alcalino terrosi)", "elementi del 15esimo gruppo (pnicogeni)", "elementi del gruppo XVI (calcogeni)", "elementi del gruppo XVII (alogeni)",
+    "elementi del 18esimo gruppo (gas nobili)", "lantanoidi", "attinoidi", "metalli di transizione", "metalli post-transizione", "semimetalli", "non metalli reattivi", "elementi del blocco s",
+    "elementi del blocco p", "elementi del blocco d", "elementi del blocco f", "elementi noti fin dai tempi antichi"
 ];
+
+const Elements_of_Diet = [
+    ["O", "C", "H", "N", "P", "Ca", "S", "K", "Na", "Cl", "Mg", "Fe", "Zn", "Cr", "Co", "Cu", "Mn", "Mo", "Ni", "V", "Si", "B", "Se", "F", "I", "Br"],
+    ["N", "O", "Ar", "C", "Ne", "He", "Kr", "Xe", "Rn"],
+    ["Sb", "Ba", "Al", "Be", "Bi", "B", "Co", "F", "Ga", "Ge", "Hf", "In", "Li", "Mg","Nb","P", "Sc","Si","Sr","Ta","Ti","W","V"],
+    ["Cu", "Al", "Ba", "Ni", "Ca", "Sn", "Fe", "Zn", "Ti", "Pb", "Ag", "Sr", "Au", "Mn", "Zr", "B", "Mg", "U", "Na", "W", "Cr", "Te", "Ge", "Ta", "Pd", "Nb", "Bi", "Ir", "Pt", "Li", "Y", "V", "Hf", "Be", "As", "In", "K", "Ga", "Co", "Sb", "Mo", "Sc", "Cd", "Re"],
+    ["C", "H", "O", "N", "P"],
+    ["U", "Th", "Pa", "Ra", "Rn", "Po", "Pb", "Bi", "Pu", "Ac", "Tl", "Am", "Np"],
+    ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Mg", "Al", "Si", "P", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Fe", "Co", "Ni", "Cu", "Ge", "Se", "Kr", "Sr", "Y", "Ru", "Sn", "Te", "I", "Xe", "Cs", "Ba", "Ce", "Nd", "Sm", "Dy", "Yb", "Lu", "Tl"],
+    ["Ge", "Sm", "Gd", "Bi", "Cm", "Es", "Fm", "Md", "No", "Lr", "Rf", "Sg", "Bh", "Mt", "Rg", "Og"],
+    ["B", "C", "F", "Na", "Al", "Si", "S", "K", "Ca", "Sc", "Mn", "Fe", "Cu", "Ga", "Ge", "Rb", "Ru", "Pd", "In", "Sn", "Sb", "Te", "Cs", "La", "Ce", "Pm", "Eu", "Ho", "Tm", "Lu", "Hf", "Ta", "Ir", "Au", "Hg", "Pb", "Bi", "Po", "Rn", "Ra", "Np", "Cm", "Hs"],
+    ["H", "He", "Li", "Be", "N", "O", "Ne", "P", "Cl", "Ar", "Ti", "Cr", "Co", "As", "Se", "Br", "Kr", "Nb", "Mo", "Tc", "Rh", "Ag", "Cd", "Sb", "I", "Xe", "Ba", "La", "Pr", "Nd", "Dy", "Os", "Tl", "Bi", "At", "Ac", "Pa", "U", "Pu"],
+    ["Mg", "Sc", "Mn", "Ga", "Ge", "Se", "Sr", "Y", "Nb", "Tc", "Ru", "Pd", "Cd", "Te", "Eu", "Tb", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Re", "Bi", "Po", "Fr", "U", "Np", "Am", "Bk", "Cf", "Db", "Hs", "Ds"],
+    ["V", "Ni", "Zn", "Zr", "Sb", "W", "Pt", "Th"],
+    ["Li", "Be", "B", "C", "Na", "Mg", "Al", "Si", "P", "S",  "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Tl", "Pb", "Bi", "Po", "At", "Fr", "Ra", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"],
+    ["Hg", "Br"],
+    ["H", "He", "N", "O", "F", "Ne","Cl", "Ar", "Kr", "Xe", "Rn"],
+    ["Li", "Be", "Na", "Mg", "Al", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"],
+    ["H", "He", "B", "C", "N", "O", "F", "Ne", "Si", "P", "S", "Cl", "Ar", "Ge", "As", "Se", "Br", "Kr", "Sb", "Te", "I", "Xe", "At", "Rn"],
+    ["H", "Li", "Na", "K", "Rb", "Cs", "Fr"],
+    ["Be", "Mg", "Ca", "Sr", "Ba", "Ra"],
+    ["N", "P", "As", "Sb", "Bi", "Mc"],
+    ["O", "S", "Se", "Te", "Po", "Lv"],
+    ["F", "Cl", "Br", "I", "At", "Ts"],
+    ["He", "Ne","Ar", "Kr", "Xe", "Rn", "Og"],
+    ["La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu"],
+    ["Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"],
+    ["Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn"],
+    ["Al", "Ga", "In", "Sn", "Tl", "Pb", "Bi", "Po"],
+    ["B", "Si", "Ge", "As", "Sb", "Te", "At"],
+    ["H", "C", "N", "O", "F", "P", "S", "Cl", "Se", "Br", "I"],
+    ["H", "He", "Li", "Be", "Na", "Mg", "K", "Ca", "Rb", "Sr", "Cs", "Ba", "Fr", "Ra"],
+    ["B", "C", "N", "O", "F", "Ne", "Al", "Si", "P", "S", "Cl", "Ar", "Ga", "Ge", "As", "Se", "Br", "Kr", "In", "Sn", "Sb", "Te", "I", "Xe", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og"],
+    ["Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn"],
+    ["La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No"],
+    ["C", "Al", "S", "Ca", "Fe", "Cu", "As", "Ag", "Sn", "Sb", "Au", "Hg", "Pb"]#https://pubchem.ncbi.nlm.nih.gov/periodic-table/#property=YearDiscovered
+]
+
 
 // Initialize game state
 const SPEED = 70;
@@ -123,12 +155,30 @@ function generateFood() {
     foodElementNumber = elementNumbers[elementIndex];
 
     // Display the current element name next to the score
-    document.getElementById('scoreBoard').textContent = `Score: ${score} | ${foodElementNumber}, ${foodElementName}`;
+    document.getElementById('scoreBoard').textContent = `Score: ${score} |  ${foodElementName} (Z ${foodElementNumber})`;
 }
 
 // Function to update score
 function updateScore(newScore) {
-    document.getElementById('scoreBoard').textContent = `Score: ${newScore} | ${foodElementNumber}, ${foodElementName}`;
+    // Ottieni l'indice della dieta selezionata
+    const dietIndex = DietsList.indexOf(selected_diet);
+
+    // Verifica se l'elemento mangiato appartiene alla dieta selezionata
+    if (Elements_of_Diet[dietIndex].includes(element_eaten)) {
+        // Aggiungi punteggio positivo se l'elemento fa parte della dieta selezionata
+        score += ((elements.length - Elements_of_Diet[dietIndex].length) / elements.length) * 100;
+    } else {
+        // Sottrai punteggio se l'elemento non appartiene alla dieta selezionata
+        score -= (Elements_of_Diet[dietIndex].length / elements.length) * 100;
+    }
+
+    // Aggiorna il punteggio visualizzato
+    updateScoreDisplay();
+}
+
+// Funzione per aggiornare la visualizzazione del punteggio
+function updateScoreDisplay() {
+    document.getElementById('scoreBoard').textContent = `Score: ${score.toFixed(2)} |  ${foodElementName} (Z ${foodElementNumber});
 }
 
 // Example function to start game loop
