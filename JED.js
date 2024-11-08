1b0640c19bcba91c9a99e785260ee5dae097c593
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-11-09 00:14:57 +0100

    Update JED.js

--- JED.js
+++ JED.js
@@ -15,15 +15,57 @@ const elementNumbers = [
 
 // List of diet options and elements associated with each diet
 const DietsList = [
-    "elementi della vita", "elemeni dell'aria", "elementi critici", "elementi degli smartphone", "elementi del DNA", "elementi radioattivi (serie di decadimento U-Th)",
-    "elementi considerati sicuri (grado A-E)\n in un reattore nucleare a fusione", "elementi dedicati a scienziati", "elementi con nome di derivazione latina",
-    "elementi con nome di derivazione greca", "elementi con nomi di città,\nstati, o di chissà dove", "elementi con nomi che non derivano nè dal latino, nè dal greco,\ne nemmeno da città o stati",
-    "elementi allo stato solido alle condizioni standard di temperatura e pressione", "elementi allo stato liquido alle condizioni standard di temperatura e pressione",
-    "elementi allo stato gassoso alle condizioni standard di temperatura e pressione", "metalli", "non metalli", "elementi del primo gruppo (idrogeno & metalli alcalini)",
-    "elementi del secondo gruppo (metalli alcalino terrosi)", "elementi del 15esimo gruppo (pnicogeni)", "elementi del gruppo XVI (calcogeni)", "elementi del gruppo XVII (alogeni)",
-    "elementi del 18esimo gruppo (gas nobili)", "lantanoidi", "attinoidi", "metalli di transizione", "metalli post-transizione", "semimetalli", "non metalli reattivi", "elementi del blocco s",
-    "elementi del blocco p", "elementi del blocco d", "elementi del blocco f", "elementi noti fin dai tempi antichi"
+    "elementi della vita", "elemeni dell'aria", "elementi critici", "elementi degli smartphone", "elementi del DNA",
+    "elementi radioattivi (serie di decadimento U-Th)", "elementi considerati sicuri (grado A-E)\n in un reattore nucleare a fusione",
+    "elementi dedicati a scienziati", "elementi con nome di derivazione latina", "elementi con nome di derivazione greca",
+    "elementi con nomi di città,\nstati, o di chissà dove",
+    "elementi con nomi che non derivano nè dal latino, nè dal greco,\ne nemmeno da città o stati",
+    "elementi allo stato solido alle condizioni standard di temperatura e pressione",
+    "elementi allo stato liquido alle condizioni standard di temperatura e pressione",
+    "elementi allo stato gassoso alle condizioni standard di temperatura e pressione", "metalli", "non metalli", 
+    "elementi del primo gruppo (idrogeno & metalli alcalini)", "elementi del secondo gruppo (metalli alcalino terrosi)", 
+    "elementi del 15esimo gruppo (pnicogeni)", "elementi del gruppo XVI (calcogeni)", "elementi del gruppo XVII (alogeni)",
+    "elementi del 18esimo gruppo (gas nobili)", "lantanoidi", "attinoidi", "metalli di transizione", "metalli post-transizione", 
+    "semimetalli", "non metalli reattivi", "elementi del blocco s", "elementi del blocco p", "elementi del blocco d", "elementi del blocco f", 
+    "elementi noti fin dai tempi antichi"
 ];
+
+const Elements_of_Diet = {
+    ["O", "C", "H", "N", "P", "Ca", "S", "K", "Na", "Cl", "Mg", "Fe", "Zn", "Cr", "Co", "Cu", "Mn", "Mo", "Ni", "V", "Si", "B", "Se", "F", "I", "Br"],
+    ["N", "O", "Ar", "C", "Ne", "He", "Kr", "Xe", "Rn"],
+    ["Sb", "Ba", "Al", "Be", "Bi", "B", "Co", "F", "Ga", "Ge", "Hf", "In", "Li", "Mg","Nb","P", "Sc","Si","Sr","Ta","Ti","W","V"],
+    ["Cu", "Al", "Ba", "Ni", "Ca", "Sn", "Fe", "Zn", "Ti", "Pb", "Ag", "Sr", "Au", "Mn", "Zr", "B", "Mg", "U", "Na", "W", "Cr", "Te", "Ge", "Ta", "Pd", "Nb", "Bi", "Ir", "Pt", "Li", "Y", "V", "Hf", "Be", "As", "In", "K", "Ga", "Co", "Sb", "Mo", "Sc", "Cd", "Re"],
+    ["C", "H", "O", "N", "P"],
+    ["U", "Th", "Pa", "Ra", "Rn", "Po", "Pb", "Bi", "Pu", "Ac", "Tl", "Am", "Np"],
+    ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Mg", "Al", "Si", "P", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Fe", "Co", "Ni", "Cu", "Ge", "Se", "Kr", "Sr", "Y", "Ru", "Sn", "Te", "I", "Xe", "Cs", "Ba", "Ce", "Nd", "Sm", "Dy", "Yb", "Lu", "Tl"],
+    ["Ge", "Sm", "Gd", "Bi", "Cm", "Es", "Fm", "Md"],
+    ["B", "C", "F", "Na", "Al", "Si", "S", "K", "Ca", "Sc", "Mn", "Fe", "Cu", "Ga", "Ge", "Rb", "Ru", "Pd", "In", "Sn", "Sb", "Te", "Cs", "La", "Ce", "Pm", "Eu", "Ho", "Tm", "Lu", "Hf", "Ta", "Ir", "Au", "Hg", "Pb", "Bi", "Po", "Rn", "Ra", "Np", "Cm",],
+    ["H", "He", "Li", "Be", "N", "O", "Ne", "P", "Cl", "Ar", "Ti", "Cr", "Co", "As", "Se", "Br", "Kr", "Nb", "Mo", "Tc", "Rh", "Ag", "Cd", "Sb", "I", "Xe", "Ba", "La", "Pr", "Nd", "Dy", "Os", "Tl", "Bi", "At", "Ac", "Pa", "U", "Pu"],
+    ["Mg", "Sc", "Mn", "Ga", "Ge", "Se", "Sr", "Y", "Nb", "Tc", "Ru", "Pd", "Cd", "Te", "Eu", "Tb", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Re", "Bi", "Po", "Fr", "U", "Np", "Am", "Bk", "Cf"],
+    ["V", "Ni", "Zn", "Zr", "Sb", "W", "Pt", "Th"],
+    ["Li", "Be", "B", "C", "Na", "Mg", "Al", "Si", "P", "S",  "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Tl", "Pb", "Bi", "Po", "At", "Fr", "Ra", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md"],
+    ["Hg", "Br"],
+    ["H", "He", "N", "O", "F", "Ne","Cl", "Ar", "Kr", "Xe", "Rn"],
+    ["Li", "Be", "Na", "Mg", "Al", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md"],
+    ["H", "He", "B", "C", "N", "O", "F", "Ne", "Si", "P", "S", "Cl", "Ar", "Ge", "As", "Se", "Br", "Kr", "Sb", "Te", "I", "Xe", "At", "Rn"],
+    ["H", "Li", "Na", "K", "Rb", "Cs", "Fr"],
+    ["Be", "Mg", "Ca", "Sr", "Ba", "Ra"],
+    ["N", "P", "As", "Sb", "Bi"],
+    ["O", "S", "Se", "Te", "Po"],
+    ["F", "Cl", "Br", "I", "At"],
+    ["He", "Ne","Ar", "Kr", "Xe", "Rn"],
+    ["La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu"],
+    ["Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md"],
+    ["Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg"],
+    ["Al", "Ga", "In", "Sn", "Tl", "Pb", "Bi", "Po"],
+    ["B", "Si", "Ge", "As", "Sb", "Te", "At"],
+    ["H", "C", "N", "O", "F", "P", "S", "Cl", "Se", "Br", "I"],
+    ["H", "He", "Li", "Be", "Na", "Mg", "K", "Ca", "Rb", "Sr", "Cs", "Ba", "Fr", "Ra"],
+    ["B", "C", "N", "O", "F", "Ne", "Al", "Si", "P", "S", "Cl", "Ar", "Ga", "Ge", "As", "Se", "Br", "Kr", "In", "Sn", "Sb", "Te", "I", "Xe", "Tl", "Pb", "Bi", "Po", "At", "Rn"],
+    ["Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg"],
+    ["La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md"],
+    ["C", "Al", "S", "Ca", "Fe", "Cu", "As", "Ag", "Sn", "Sb", "Au", "Hg", "Pb"]
+};
 
 // Initialize game state
 const SPEED = 90;
@@ -33,6 +75,8 @@ const CANVAS_HEIGHT = 520;
 let gameInterval = null;
 let direction = { x: 1, y: 0 };
 let score = 0;
+let selectedDietIndex = 0; // Index of selected diet, for example, "elementi della vita"
+let eatenElement = "O"; // The element eaten by the snake
 let foodElement = "";
 let foodElementName = "";
 let foodElementNumber = "";
@@ -57,16 +101,16 @@ document.addEventListener('keydown', (event) => {
             break;
         case 'ArrowRight':
             if (direction.x === 0) direction = { x: 1, y: 0 };
-         case 'W':
+         case 'w':
             if (direction.y === 0) direction = { x: 0, y: -1 };
             break;
-        case 'S':
+        case 's':
             if (direction.y === 0) direction = { x: 0, y: 1 };
             break;
-        case 'A':
+        case 'a':
             if (direction.x === 0) direction = { x: -1, y: 0 };
             break;
-        case 'D':
+        case 'd':
             if (direction.x === 0) direction = { x: 1, y: 0 };       
             break;
     }
@@ -160,7 +204,11 @@ function updateGame(ctx) {
             
     // Controlla se il serpente mangia il cibo
     if (head.x === food.x && head.y === food.y) {
-        score += 10;  // Incrementa il punteggio
+        if (Elements_of_Diet[selectedDietIndex].includes(eatenElement)) {
+            score += 10;
+        } else {
+            score -= 5;
+}
         updateScore(score);
         generateFood(); // Genera un nuovo cibo
     } else {
