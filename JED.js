047abdc549de7a7a3f2f79631bce3997e20b53e6
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-11-09 12:06:40 +0100

    Update JED.js

--- JED.js
+++ JED.js
@@ -124,7 +124,7 @@ function startGameLoop(ctx) {
 }
 
 function updateScore(newScore) {
-    document.getElementById('scoreBoard').textContent = `${selectedDiet}\nScore: ${newScore} | ${foodElementName}, ${foodElement} (Z = ${foodElementNumber})`;
+    document.getElementById('scoreBoard').textContent = `${selectedDiet}<br>Score: ${newScore} | ${foodElementName}, ${foodElement} (Z = ${foodElementNumber})`;
 }
 
 function updateGame(ctx) {
