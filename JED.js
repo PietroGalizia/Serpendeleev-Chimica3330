d89a1d467c99b094c0947bafb82ae0f2f8f7998d
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-11-09 21:45:52 +0100

    Update JED.js

--- JED.js
+++ JED.js
@@ -15,7 +15,7 @@ const elementNumbers = [
 
 // List of diet options and elements associated with each diet
 const DietsList = [
-    "elements of life", "critical raw elements", "elements of a smartphone", 
+    "elements of life", "critical raw elements", "elements of a smartphone",
     "elements of DNA", "radioactive elements (U-Th decay series)",
     "elements considered safety (grades A-E) in the first wall of fusion power plan",
     "elements dedicated to scientists", "elements with names of latin derivation",
@@ -33,7 +33,7 @@ const DietsList = [
     "s-block elements", "p-block elements", "d-block elements", "f-block elemnts"
 ];
 
-const diets = {
+onst diets = {
     "elements of life": ["O", "C", "H", "N", "P", "Ca", "S", "K", "Na", "Cl", "Mg", "Fe", "Zn", "Cr", "Co", "Cu", "Mn", "Mo", "Ni", "V", "Si", "B", "Se", "F", "I", "Br"],
     "critical raw elements": ["Sb", "Ba", "Al", "Be", "Bi", "B", "Co", "F", "Ga", "Ge", "Hf", "In", "Li", "Mg","Nb","P", "Sc","Si","Sr","Ta","Ti","W","V"],
     "elements of a smartphone": ["Cu", "Al", "Ba", "Ni", "Ca", "Sn", "Fe", "Zn", "Ti", "Pb", "Ag", "Sr", "Au", "Mn", "Zr", "B", "Mg", "U", "Na", "W", "Cr", "Te", "Ge", "Ta", "Pd", "Nb", "Bi", "Ir", "Pt", "Li", "Y", "V", "Hf", "Be", "As", "In", "K", "Ga", "Co", "Sb", "Mo", "Sc", "Cd", "Re"],
