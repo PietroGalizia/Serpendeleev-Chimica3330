85574437d294ce0c9f82aabd78c539d9b53aa32f
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2025-02-04 23:27:13 +0100

    Update JED.js

--- JED.js
+++ JED.js
@@ -495,11 +495,12 @@ function generateFood() {
     }
     
     let elementIndex;
+    // Continua a generare un elemento finché non ne trovi uno che non è in erasedElements
     do {
         elementIndex = Math.floor(Math.random() * elements.length);
         foodElement = elements[elementIndex];
     } while (erasedElements.includes(foodElement));
-    
+
     // Assegna nome e numero dell’elemento selezionato
     foodElementName = elementNames[elementIndex];
     foodElementNumber = elementNumbers[elementIndex];
@@ -528,11 +529,13 @@ function generateFoodII() {
        !(foodII.x === food.x && foodII.y === food.y);
     }
     
-    let elementIndex
+    let elementIndex;
+    // Continua a generare un elemento finché non ne trovi uno che non è in erasedElements
+    do {
         elementIndex = Math.floor(Math.random() * elements.length);
         foodIIElement = elements[elementIndex];
     } while (erasedElements.includes(foodIIElement));
-    
+
     // Assegna nome e numero dell’elemento selezionato
     foodIIElementName = elementNames[elementIndex];
     foodIIElementNumber = elementNumbers[elementIndex];
